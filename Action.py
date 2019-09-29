c0f052b3933446d46b1ae346619def89ddaa17a1
kevin1019 <kevinrizkimohammad@gmail.com>
2019-09-29 12:20:08 +0700

    Add Action.py

---
+++ Action.py
@@ -0,0 +1,35 @@
+def TidurSiang():
+
+def TidurMalam():
+
+def MakanHamburger():
+
+def MakanPizza():
+
+def MakanSteakAndBans():
+
+def MinumAir():
+
+def MinumKopi():
+
+def MinumJus():
+
+def BuangAirKecil():
+
+def BuangAirBesar():
+
+def BersosialisasiKeKafe():
+
+def BermainMediaSosial():
+
+def BermainKomputer():
+
+def Mandi():
+
+def CuciTangan():
+
+def MendengarkanMusikDiRadio():
+
+def MembacaKoran():
+
+def MembacaNovel():    
