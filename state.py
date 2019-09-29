1aaabdf6407b03ab9d35b386259b805f8e10e3ab
fabianzhafran <fabianzhafransyahharahap@gmail.com>
2019-09-29 12:18:37 +0700

    Initial commit, added state to the code

---
+++ state.py
@@ -0,0 +1,12 @@
+hygiene = int(0)
+energy = int(10)
+fun = int(10)
+
+state = [[["" for k in range(4)] for j in range(4)] for i in range(4)]
+
+for i in range(len(state) - 1):
+    for j in range(len(state[i]) - 1):
+        for k in range(len(state[i][j]) - 1):
+            state[i][j][k] = "State (" + str(i * 5) + " ," + str(j * 5) + ", " + str(k * 5) + ")"
+
+print(state[1][1][1])
